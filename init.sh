6d72077759fce80844c2b81145a57e341d053e8d
chao23387784 <chao23387784@163.com>
2024-05-14 10:05:17 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git init
+git config --global user.name "chao23387784"
+git config --global user.email "chao23387784@163.com"
+git remote add origin https://github.com/chao23387784/linux.git
