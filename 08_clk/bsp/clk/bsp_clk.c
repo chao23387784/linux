e443da702a8df4d5391e20eddf3c7316c34fef12
chao23387784 <chao23387784@163.com>
2024-05-20 15:51:06 +0800

    update clock code

--- 08_clk/bsp/clk/bsp_clk.c
+++ 08_clk/bsp/clk/bsp_clk.c
@@ -14,5 +14,21 @@ void clk_enable(void)
 
 void imx6u_clkinit()
 {
-	
+	/* init arm clk to 528MHz*/
+	if((CCM->CCSR >> 2 & 0x01) == 0)
+	{
+		CCM->CCSR |= (1 << 2);
+	}
+
+	CCM_ANALOG->PLL_ARM &= ~(0x7F << 0);
+	CCM_ANALOG->PLL_ARM |= 88;
+
+	CCM->CACRR &= ~(0x07 << 0);
+	CCM->CACRR |= 0x01;
+
+	uint32_t reg = CCM_ANALOG->PFD_528;
+	reg &= ~(0x1F << 0);
+	reg |= 27;//PLL2 PFD0 352MHz
+
+	reg &= ~(0x1F << 8);
 }
