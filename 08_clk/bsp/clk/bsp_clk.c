bd4dfddef68f4113e1a0d16bf847c72de784eb87
chao23387784 <chao23387784@163.com>
2024-05-21 11:15:34 +0800

    update clock code

--- 08_clk/bsp/clk/bsp_clk.c
+++ 08_clk/bsp/clk/bsp_clk.c
@@ -15,20 +15,42 @@ void clk_enable(void)
 void imx6u_clkinit()
 {
 	/* init arm clk to 528MHz*/
-	if((CCM->CCSR >> 2 & 0x01) == 0)
+	if((CCM->CCSR >> 2 & 0x01) == 0)//is arm clk switch to main?
 	{
-		CCM->CCSR |= (1 << 2);
+		CCM->CCSR |= (1 << 2);//switch to soc
 	}
 
 	CCM_ANALOG->PLL_ARM &= ~(0x7F << 0);
-	CCM_ANALOG->PLL_ARM |= 88;
+	CCM_ANALOG->PLL_ARM |= 88;//set arm clk total freq
 
 	CCM->CACRR &= ~(0x07 << 0);
-	CCM->CACRR |= 0x01;
+	CCM->CACRR |= 0x01;//set devide value
 
 	uint32_t reg = CCM_ANALOG->PFD_528;
 	reg &= ~(0x1F << 0);
-	reg |= 27;//PLL2 PFD0 352MHz
+	reg |= (27 << 0);//PLL2 PFD0 352MHz
 
 	reg &= ~(0x1F << 8);
+	reg |= (16 << 8);//PLL2 PFD1 594MHz
+
+	reg &= ~(0x1F << 16);
+	reg |= (24 << 16);//PLL2 PFD2 396MHz
+
+	reg &= ~(0x1F << 24);
+	reg |= (48 << 24);//PLL2 PFD3 200MHz 48 out of range of 12-35!!!!!!!
+	CCM_ANALOG->PFD_528 = reg;
+
+	reg = CCM_ANALOG->PFD_480;
+	reg &= ~(0x1F << 0);
+	reg |= (12 << 0);//PLL3 PFD0 720MHz
+
+	reg &= ~(0x1F << 8);
+	reg |= (16 << 8);//PLL3 PFD1 540MHz
+
+	reg &= ~(0x1F << 16);
+	reg |= (17 << 16);//PLL3 PFD2 508.2MHz
+
+	reg &= ~(0x1F << 24);
+	reg |= (19 << 24);//PLL3 PFD3 454.7MHz	
+	CCM_ANALOG->PFD_480 = reg;
 }
